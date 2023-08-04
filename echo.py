0c29f4807f40c870368efd320b173afce78ce8d9
Ankit <ankithooda23@gmail.com>
2023-08-04 14:24:32 +0530

    Fixed echo server

--- echo.py
+++ echo.py
@@ -11,7 +11,7 @@ class EchoServer:
         self.node_id = node_id
         self.msg_counter = 0
 
-    def send_reply(self, payload, reply_type):
+    def send_reply(self, req_payload, body):
 
         # {src: "n1",
         #  dest: "c1",
@@ -19,43 +19,44 @@ class EchoServer:
         #         in_reply_to: 1
         #         type: "init_ok"}}
 
-        # Increment message counter first.
-        self.msg_counter = self.msg_counter + 1
 
         # Construct reply body
         reply_msg = {'body': {}}
+        reply_msg['id'] = self.msg_counter
         reply_msg['src'] = self.node_id
-        reply_msg['dest'] = payload['src']
-        reply_msg['body']['msg_id'] = self.msg_counter
-        reply_msg['body']['in_reply_to'] = payload['id']
+        reply_msg['dest'] = req_payload['src']
+        reply_msg['body'] = body
 
         # Add the reply type
-        reply_msg['body']['type'] = reply_type
-
         sys.stderr.write(json.dumps(reply_msg) + "\n")
         sys.stdout.write(json.dumps(reply_msg) + "\n")
+        sys.stdout.flush()
+        
+        # Increment message counter first.
+        self.msg_counter = self.msg_counter + 1
 
-    def reply_to_init(self, payload):
+    def handle_init(self, payload):
         self.node_id = payload['body']['node_id']
-        self.send_reply(payload, "init_ok")
+        sys.stderr.write(f"Initialized node with id {self.node_id}" + "\n")
+
+        body = {
+            "in_reply_to": payload['body']['msg_id'],
+            "type": "init_ok"
+        }
+        self.send_reply(payload, body)
 
     def run(self):
         for line in sys.stdin:
             try:
                 payload = json.loads(line)
-
-                sys.stderr.write("\n" + str(payload) + "\n")
-
+                sys.stderr.write(f"Got {str(payload)}\n")
                 if payload['body']['type'] == "init":
-                    self.reply_to_init(payload)
-                    
+                    self.handle_init(payload)
                 else:
                     sys.stderr.write("\nSome other message type received \n")
-
-                sys.stderr.write("\nDONE.\n")
-
             except Exception as e:
                 sys.stderr.write(str(e))
 
 e = EchoServer()
 e.run()
+clownworld_
